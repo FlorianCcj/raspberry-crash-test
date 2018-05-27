dc1ec332dd6d1407f9092549822b1618ba805451
florianccj <florianccj@gmail.com>
2018-05-27 11:26:33 +0200

    [*][button reading] test variable and print

--- 02-GPIO-Python/02-Button-reading/Button.py
+++ 02-GPIO-Python/02-Button-reading/Button.py
@@ -4,20 +4,26 @@
 import RPi.GPIO as GPIO
 import time
 
+LED_INPUT_PIN = 12
+BUTTON_OUTPUT_PIN = 19
+print("led's input's pin: " + LED_INPUT_PIN)
+print("button's output's pin: " + BUTTON_OUTPUT_PIN)
+
 # Initialisation de la numerotation et des E/S
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LED_INPUT_PIN, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(19, GPIO.IN)
 
 # Si on detecte un appui sur le bouton, on allume la LED 
 # et on attend que le bouton soit relache
-while False:
+while True:
     state = GPIO.input(19)
     if not state:
         # on a appuye sur le bouton connecte sur la broche 19
-        GPIO.output(12, GPIO.HIGH)
+        GPIO.output(LED_INPUT_PIN, GPIO.HIGH)
         while not state:
+            print("button is close")
             state = GPIO.input(19)
             time.sleep(0.02)  # Pause pour ne pas saturer le processeur
-        GPIO.output(12, GPIO.LOW)
+        GPIO.output(LED_INPUT_PIN, GPIO.LOW)
     time.sleep(0.02)  # Pause pour ne pas saturer le processeur
