bbd7c588ebeb3be73de941f9d46e9062cee6eda3
florianccj <florianccj@gmail.com>
2018-05-27 12:09:26 +0200

    [*][button reading] clean

--- 02-GPIO-Python/02-Button-reading/Button.py
+++ 02-GPIO-Python/02-Button-reading/Button.py
@@ -25,12 +25,10 @@ GPIO.setup(pin.output_button, GPIO.IN)
 # et on attend que le bouton soit relache
 while True:
     state = GPIO.input(pin.output_button)
-    print(state)
-    if state:
+    if not state:
         # on a appuye sur le bouton connecte sur la broche 19
         GPIO.output(pin.input_led, GPIO.HIGH)
-        while state:
-            print("button is close")
+        while not state:
             state = GPIO.input(pin.output_button)
             time.sleep(0.02)  # Pause pour ne pas saturer le processeur
         GPIO.output(pin.input_led, GPIO.LOW)
