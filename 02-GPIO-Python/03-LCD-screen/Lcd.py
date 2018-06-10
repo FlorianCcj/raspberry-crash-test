b06f456fc113b8e5fdbbbdd3a5a8db7d5f6bb2cb
florianccj <florianccj@gmail.com>
2018-06-10 16:54:59 +0200

    add clean

--- 02-GPIO-Python/03-LCD-screen/Lcd.py
+++ 02-GPIO-Python/03-LCD-screen/Lcd.py
@@ -3,6 +3,7 @@
 import RPi.GPIO
 import Adafruit_CharLCD
 
+GPIO.cleanup()
 lcd = Adafruit_CharLCD.Adafruit_CharLCD(pin_rs=14, pin_e=15, pins_db=[2,3,4,17])
 lcd.clear()
 lcd.message("Hello world !!!")
